7433cffef7c2976cf24510029e71b6c7f01e1bc2
Tomas <tomas@ubuntu.(none)>
2011-11-12 02:27:16 +0100

    first commit with oauth a medias

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialTour::Application.config.secret_token = '7b9025c9d7ae42f694fec4e4767f0f6d49d4caf413d0a4406a5bfcfd9b58690295dc41d48d654f1ac5976c5631a168c37e8ce8acd25aaa0daa7f9f2077a5c0f8'
