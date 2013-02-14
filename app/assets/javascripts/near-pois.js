35c4624fa3dfd23a332b6bb5d53712fa54f7eb94
Tomas Madariaga <critimass@gmail..com>
2013-02-15 00:20:17 +0100

    geolocalization added

--- app/assets/javascripts/near-pois.js
+++ app/assets/javascripts/near-pois.js
@@ -25,14 +25,42 @@ $(function(){
   }
   
   function initialize() {
+  
     var options = {
       scrollwheel: false,
       zoom: 12,
-      center: new google.maps.LatLng(43.28,-3),
+      /*center: new google.maps.LatLng(43.28,-3),*/
       mapTypeId: google.maps.MapTypeId.HYBRID
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), options);
-  
+    
+    // Try W3C Geolocation (Preferred)
+	  if(navigator.geolocation) {
+	    browserSupportFlag = true;
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	      map.setCenter(initialLocation);
+	    }, function() {
+	      handleNoGeolocation(browserSupportFlag);
+	    });
+	  }
+	  // Browser doesn't support Geolocation
+	  else {
+	    browserSupportFlag = false;
+	    handleNoGeolocation(browserSupportFlag);
+	  }
+	  
+	  function handleNoGeolocation(errorFlag) {
+	    if (errorFlag == true) {
+	      alert("Geolocation service failed.");
+	      initialLocation = newyork;
+	    } else {
+	      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+	      initialLocation = siberia;
+	    }
+	    map.setCenter(initialLocation);
+	  }
+	  
     $.each(pois, function(index, poi) {
       markers.push(createMarker(poi));
     });
@@ -40,4 +68,3 @@ $(function(){
   google.maps.event.addDomListener(window, 'load', initialize);
 
 });
-
